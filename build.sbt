fc59ba423f91c2657430c08ae5729fa1b74bffb2
FENG <38035703+HuitingFENG@users.noreply.github.com>
2023-06-27 13:54:32 +0200

    update all

--- build.sbt
+++ build.sbt
@@ -1,18 +1,7 @@
-
-// The simplest possible sbt build file is just one line:
-
-/* scalaVersion := "2.13.8"
-
-name := "hello-world"
-organization := "ch.epfl.scala"
-version := "1.0"
-
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
- */
-
 val zioVersion = "2.0.15"
 val scala3Version = "3.3.0"
 
+ 
 lazy val root = project
   .in(file("."))
   .settings(
@@ -24,6 +13,9 @@ lazy val root = project
     libraryDependencies ++= Seq(
       "dev.zio" %% "zio" % zioVersion,
       // Add other libraries like zio-nio and zip-json here if needed
+      "dev.zio" %% "zio-json" % "0.5.0",
+      "dev.zio" %% "zio-nio" % "2.0.1",
+
     ).map(_ % Compile),
     libraryDependencies ++= Seq(
       "org.scalameta" %% "munit" % "0.7.29"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,41 @@
-package sudoku
-
 import zio._
+import zio.Console._
+import zio.json._
+import java.nio.charset.StandardCharsets
+import zio.nio.file.{Path, Files}
+
+ 
 
 object Main extends ZIOAppDefault {
 
-  def run: ZIO[Any, Throwable, Unit] =
+ 
+
+  def readJsonString(path: String): ZIO[Any, Throwable, String] =
+    Files
+      .readAllBytes(Path(path))
+      .map(bytes => new String(bytes.toArray, StandardCharsets.UTF_8))
+
+ 
+
+  def parseSudoku(jsonString: String): ZIO[Any, Throwable, List[List[Option[Int]]]] =
+  jsonString.fromJson[List[List[Option[Int]]]] match {
+    case Left(error)     => ZIO.fail(new RuntimeException(error))
+    case Right(sudoku)   => ZIO.succeed(sudoku)
+  }
+
+
+//two functions, the first to read the json as chaine de caractere
+//the second to parse the chaine de caractere
+
+ 
+
+  def run: ZIO[Any, Any, Unit] =
     for {
       _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
       path <- Console.readLine
-      _ <-  Console.printLine(s"You entered: $path")
-      // Add your Sudoku solver logic here, utilizing ZIO and interacting with the ZIO Console
-
-      
+      _ <- Console.printLine(s"You entered: $path")
+      jsonString <- readJsonString(path)
+      sudoku <- parseSudoku(jsonString)
+      // Use the 'sudoku' list in your Sudoku solver logic
     } yield ()
 }
